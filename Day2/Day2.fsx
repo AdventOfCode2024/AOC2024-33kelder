3811a693c6e9c4f6e6b5fa7f21016d05fad004a6
Ronald Eekelder <info@oakrefactory.com>
2024-12-02 17:32:38 +0100

    Day2 part 2

--- Day2/Day2.fsx
+++ Day2/Day2.fsx
@@ -3,8 +3,8 @@ open System.IO
 
 let inputPath = Path.Combine(__SOURCE_DIRECTORY__, "Input.txt")
 let inputLines = File.ReadAllLines(inputPath)
-
-let inputLinesTest = [| "7 6 4 2 1";
+let inputLinesTest = [|
+    "7 6 4 2 1";
     "1 2 7 8 9";
     "9 7 6 2 1";
     "1 3 2 4 5";
@@ -12,34 +12,45 @@ let inputLinesTest = [| "7 6 4 2 1";
     "1 3 6 7 9";
 |]
 
-let onlyIncreasingOrDecreasing (pairs: (int*int)[]) =
-    let allIncreasing = 
-        pairs
-        |> Array.map (fun (n1, n2) -> n2 - n1 > 0)
-        |> Array.forall id
-    let allDecreasing = 
-        pairs
-        |> Array.map (fun (n1, n2) -> n1 - n2 > 0)
-        |> Array.forall id
-    allIncreasing || allDecreasing
+let onlyIncreasing (pairs: (int*int) list) =
+    pairs
+    |> List.forall (fun (n1, n2) -> n2 - n1 > 0)
 
-let increaseIsSafe (n1:int, n2:int) =
+let onlyDecreasing (pairs: (int*int) list) =
+    pairs
+    |> List.forall (fun (n1, n2) -> n1 - n2 > 0)
+
+let pairIsSafe (n1:int, n2:int) =
     let increase = Math.Abs(n2 - n1)
     increase > 0 && increase < 4
-
-let reportIsSafe (line:string) = 
+let increaseIsSafe (pairs: (int*int)list) =
+    pairs
+    |> List.forall pairIsSafe
+let tryReportIsSafe (report:int list) =
     let pairs =
-        line.Split(' ')
-        |> Array.map int
-        |> Array.pairwise
-    let onlyIncreasingOrDecreasing = onlyIncreasingOrDecreasing pairs
-    let increaseIsSafe =
-        pairs
-        |> Array.map increaseIsSafe
-        |> Array.forall (fun b -> b = true)
-    onlyIncreasingOrDecreasing && increaseIsSafe
+        report
+        |> List.pairwise
+    let onlyIncreasing = onlyIncreasing pairs
+    let onlyDecreasing = onlyDecreasing pairs
+    let increaseIsSafe = increaseIsSafe pairs
+    (onlyIncreasing || onlyDecreasing) && increaseIsSafe
+let reportIsSafe (report:string) =
+    let levels =
+        report.Split(' ')
+        |> Array.toList
+        |> List.map int
+    if tryReportIsSafe(levels) then
+        true
+    else
+        let isNotSafe =
+            [0..levels.Length - 1]
+            |> List.forall (fun index ->
+                let r = List.removeAt index levels
+                not (tryReportIsSafe r)
+                )
+        not isNotSafe
 
 inputLines
 |> Array.map reportIsSafe
-|> Array.filter (fun b -> b)
+|> Array.filter id
 |> Array.length
